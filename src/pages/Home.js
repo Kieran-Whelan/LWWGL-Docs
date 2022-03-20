ec124f8be9eb2b48d7fd7ae14de61af35a4f46bf
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-03-20 18:54:35 +0000

    added some content to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function Home () {
     return (
-        <div>
-            <h1>Introduction</h1>
+        <div className="Home">
+          <div>
+              <h1>What is LWWGL?</h1>
+          </div>
+            <hr />
+          <div>
+            <p>LWWGL stands for lightweight web game library. It's a game library I made with javascript for html canvas games. LWWGL contains a collection of Objects and helper functions for creating 2D browser games without writing thr boring boilerplate code</p>
+          </div>
         </div>
     );
 }

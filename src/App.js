44c9684937f934fdf94e3fdac7f29a2c95cf04b9
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-08-15 23:54:43 +0100

    removed unused dependencies

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Install from './components/pages/Install';
 import Docs from './components/pages/Docs';
 import NavBar from './components/NavBar';
 import LWWGL from './res/LWWGL.png'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import SearchBar from './components/SearchBar';
 
 const placeholder = "Search docs";
@@ -16,8 +16,14 @@ function App() {
       <div className='App'>
         <div className='App-content'>
           <div className='App-content-left'>
-            <img className='logo' src={LWWGL} alt="Logo" />
-            <SearchBar placeholder={placeholder} />
+            <div>
+              <img className='logo' src={LWWGL} alt="Logo" />
+              <SearchBar placeholder={placeholder} />
+            </div>
+            <hr />
+            <div>
+              <h1>About</h1>
+            </div>
           </div>
           <div className='App-content-right'>
 
--- src/components/CodeSample.js
+++ src/components/CodeSample.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark";
 import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
 
 class CodeSample extends React.Component {
